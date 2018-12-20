70188dd4bcda700f73ea08439429cb8a1ee314d6
Tjark Kühl <tjark-kuehl@hotmail.com>
2018-12-20 19:42:14 +0100

    Splitted up in functions

--- index.js
+++ index.js
@@ -2,39 +2,49 @@ import path from 'path';
 import { promises as fs } from 'fs';
 import gitP from 'simple-git/promise';
 import { spawn } from 'child_process';
-//const { spawn } = require('child_process');
 
-import cfg from './config/git.json';
+import cfg from './config/daemon-config.json';
 import walk from 'walk-promise';
 
+const repoDir = path.join(__dirname, '..', '_repository');
+const dockerComposeFileDir = path.join(repoDir, cfg.dockerComposeFileDir);
+const mergeDir = path.join(__dirname, '..', '_merge');
+
+let git;
+
 (async () => {
-    const repoDir = path.join(__dirname, '_repository');
-
     /* Create default directories */
-    await fs.mkdir(repoDir, { recursive: true });
-    await fs.mkdir(path.join(__dirname, '_merge'), { recursive: true });
+    await createDir(repoDir);
+    await createDir(mergeDir);
 
-    /* Set git dir */
-    const git = gitP(repoDir);
+    git = gitP(repoDir);
 
     /* Check if the repoDir is a valid repository */
     const isRepo = await git.checkIsRepo();
-    console.log(isRepo);
     if (!isRepo) {
-        await initGitProject(cfg.repository);
+        await cloneRepo(cfg.repo.url);
     }
 
-    console.log('Pulling changes...');
-    console.log(await git.pull('origin', 'master'));
-
-    return;
-    console.log('Copy merge files...');
-    await copyMergeFiles(__dirname);
-
-    await startContainers(path.join(repoDir, 'vue'));
+    await loop();
 })();
 
-async function initGitProject(repoUrl) {
+async function loop() {
+    console.log('Pulling changes...');
+    console.log(await git.pull('origin', cfg.repo.branch));
+
+    console.log('Copy merge files...');
+    await copyMergeFiles();
+
+    await startContainers(dockerComposeFileDir);
+
+    setTimeout(loop, cfg.minPullIntervalInSec * 1000);
+}
+
+async function createDir(dirPath) {
+    await fs.mkdir(dirPath, { recursive: true });
+}
+
+async function cloneRepo(repoUrl) {
     console.log('Initializing repository');
     await git.init();
 
@@ -45,13 +55,13 @@ async function initGitProject(repoUrl) {
     // git.fetch();
 }
 
-async function copyMergeFiles(baseDir) {
-    for (const file of await walk(path.join(baseDir, '_merge'))) {
-        const fPath = file.root.replace(path.join(baseDir, '_merge'), '');
-        await fs.mkdir(path.join(baseDir, '_repository', fPath), { recursive: true });
+async function copyMergeFiles() {
+    for (const file of await walk(mergeDir)) {
+        const fPath = file.root.replace(mergeDir, '');
+        await fs.mkdir(path.join(repoDir, fPath), { recursive: true });
         await fs.copyFile(
-            path.join(baseDir, '_merge', fPath, file.name),
-            path.join(baseDir, '_repository', fPath, file.name)
+            path.join(mergeDir, fPath, file.name),
+            path.join(repoDir, fPath, file.name)
         );
         console.log(`Copy file '${file.name}'`);
     }
@@ -75,7 +85,6 @@ function startContainers(cwd) {
 
         ls.stdout.on('data', logToConsole);
         ls.stderr.on('data', logToConsole);
-
         ls.on('close', resolve);
     });
 }
